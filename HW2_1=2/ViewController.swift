dbecb1ee31eadbb88bbdc4496d6126016a921516
Tokarev Dmitry <modelist61@bk.ru>
2020-10-14 01:24:44 +0300

    Initial Commit

---
+++ HW2_1=2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2_1=2
+//
+//  Created by Dmitry Tokarev on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
