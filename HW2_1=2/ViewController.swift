d443b4b65a29205e99109aa8338a9e53586fec95
Tokarev Dmitry <modelist61@bk.ru>
2020-10-14 04:04:48 +0300

    Hard code! need change logic!

--- HW2_1=2/ViewController.swift
+++ HW2_1=2/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var changeLightsButtom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeLightsButtom.layer.cornerRadius = 10
+        redLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        greenLightView.layer.cornerRadius = 60
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
-
+    
+    
+    @IBAction func changeLights() {
+        
+        if redLightView.alpha < 1 {
+            redLightView.alpha = 1
+            changeLightsButtom.setTitle("NEXT", for: .normal)
+        } else if yellowLightView.alpha < 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            changeLightsButtom.setTitle("NEXT", for: .normal)
+        } else if greenLightView.alpha < 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            changeLightsButtom.setTitle("NEXT", for: .normal)
+        }
+        
+    }
+    
 
 }
 
